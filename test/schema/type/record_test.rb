1419035b628faa72a46098ba6b5b9b3a57dddb0a
Luiz Vasconcellos <luizpvasc@gmail.com>
2024-12-17 11:45:22 -0300

    add test for nested records

--- test/schema/type/record_test.rb
+++ test/schema/type/record_test.rb
@@ -12,6 +12,23 @@ class Schema::Type::RecordTest < ActiveSupport::TestCase
     )
   end
 
+  test "nested records" do
+    customer_schema = {
+      name: :string,
+      company: { name: :string }
+    }
+
+    assert_ok(
+      { name: "John", company: { name: "Acme Co." } },
+      validate({ name: "John", company: { name: "Acme Co." } }, customer_schema)
+    )
+
+    assert_error(
+      { company: { name: :string } },
+      validate({ name: "John", company: {} }, customer_schema)
+    )
+  end
+
   test "error" do
     assert_error(
       { age: :integer },
