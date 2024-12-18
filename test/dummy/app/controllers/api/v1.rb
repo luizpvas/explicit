f94a77336a8d2e0b1a482f90fe7b1a0e75219324
Luiz Vasconcellos <luizpvasc@gmail.com>
2024-12-18 20:05:29 -0300

    wip api documentation

--- test/dummy/app/controllers/api/v1.rb
+++ test/dummy/app/controllers/api/v1.rb
@@ -2,17 +2,17 @@
 
 class API::V1 < Schema::API::Documentation
   section "Introduction" do
-    add partial: "api/v1/introduction"
+    # add title: "Introduction", partial: "api/v1/introduction"
   end
 
   section "Auth" do
-    add API::V1::SessionsController::CreateSchema
-    add API::V1::RegistrationsController::CreateSchema
+    # add API::V1::SessionsController::CreateSchema
+    # add API::V1::RegistrationsController::CreateSchema
   end
 
   section "Posts" do
-    add API::V1::PostsController::CreateSchema
-    add API::V1::PostsController::UpdateSchema
-    add API::V1::PostsController::DestroySchema
+    # add API::V1::PostsController::CreateSchema
+    # add API::V1::PostsController::UpdateSchema
+    # add API::V1::PostsController::DestroySchema
   end
 end
