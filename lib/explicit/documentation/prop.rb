5f86d415e609b69716982f24cc8b8e6a24e94d3c
Luiz Vasconcellos <luizpvasc@gmail.com>
2024-12-30 11:43:28 -0300

    wip string spec documentation

--- lib/explicit/documentation/prop.rb
+++ lib/explicit/documentation/prop.rb
@@ -7,7 +7,7 @@ module Explicit::Documentation
       in :string
         just_name("string")
       in [:string, options]
-        with_partial("string", "spec/string", { options: })
+        with_details("string", "explicit/spec/string", { options: })
       end
     end
 
@@ -15,8 +15,16 @@ module Explicit::Documentation
       new(name:, description: nil, partial: nil, locals: nil)
     end
 
-    def self.with_partial(name, partial, locals)
+    def self.with_details(name, partial, locals)
       new(name:, description: nil, partial:, locals:)
+    end
+
+    def renders_successfully?
+      return true if partial.nil?
+
+      Explicit::ApplicationController.render(partial:, locals:)
+
+      true
     end
   end
 end
--- test/explicit/documentation/prop_test.rb
+++ test/explicit/documentation/prop_test.rb
@@ -6,6 +6,14 @@ class Explicit::Documentation::PropTest < ActiveSupport::TestCase
   Prop = Explicit::Documentation::Prop
 
   test "string" do
-    assert_equal Prop.just_name("string"), Prop.from_spec(:string)
+    Prop.from_spec(:string).tap do |prop|
+      assert prop.renders_successfully?
+      assert_equal Prop.just_name("string"), prop
+    end
+
+    Prop.from_spec([:string, empty: false]).tap do |prop|
+      assert prop.renders_successfully?
+      assert_equal Prop.with_details("string", "explicit/spec/string", options: { empty: false }), prop
+    end
   end
 end
