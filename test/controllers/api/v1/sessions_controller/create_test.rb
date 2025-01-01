a14d19653e71cd87d83352cbb6d0a6f062cfcd25
Luiz Vasconcellos <luizpvasc@gmail.com>
2025-01-01 19:39:18 -0300

    update request examples

--- test/controllers/api/v1/sessions_controller/create_test.rb
+++ test/controllers/api/v1/sessions_controller/create_test.rb
@@ -3,40 +3,54 @@
 require "test_helper"
 
 class API::V1::SessionsController::CreateTest < ActionDispatch::IntegrationTest
-  Request = API::V1::SessionsController::CreateRequest
-
   test "successful login" do
-    response = fetch(Request, params: {
-      email_address: "luiz@example.org",
-      password: "mystrongpassword"
-    })
+    response = fetch(
+      API::V1::SessionsController::CreateRequest,
+      params: {
+        email_address: "luiz@example.org",
+        password: "mystrongpassword"
+      },
+      save_as_example: true
+    )
 
     assert_equal 200, response.status
     assert response.data[:token]
   end
 
   test "email does not belong to any user" do
-    response = fetch(Request, params: {
-      email_address: "non-existing-user@example.org",
-      password: "any-password"
-    })
+    response = fetch(
+      API::V1::SessionsController::CreateRequest,
+      params: {
+        email_address: "non-existing-user@example.org",
+        password: "any-password"
+      },
+      save_as_example: true
+    )
 
     assert_equal 422, response.status
     assert_equal "invalid_credentials", response.data[:error]
   end
 
   test "wrong password" do
-    response = fetch(Request, params: {
-      email_address: "luiz@example.org",
-      password: "wrong-password"
-    })
+    response = fetch(
+      API::V1::SessionsController::CreateRequest,
+      params: {
+        email_address: "luiz@example.org",
+        password: "wrong-password"
+      },
+      save_as_example: true
+    )
 
     assert_equal 422, response.status
     assert_equal "invalid_credentials", response.data[:error]
   end
 
   test "invalid params" do
-    response = fetch(Request, params: {})
+    response = fetch(
+      API::V1::SessionsController::CreateRequest,
+      params: {},
+      save_as_example: true
+    )
     
     assert_equal 422, response.status
     assert_equal response.data, {
