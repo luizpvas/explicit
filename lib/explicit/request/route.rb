f92eaefa5cbfa6f0b607a44770ed8db491e2e7da
Luiz Vasconcellos <luizpvasc@gmail.com>
2025-01-02 17:45:31 -0300

    display example with path param

--- lib/explicit/request/route.rb
+++ lib/explicit/request/route.rb
@@ -11,5 +11,11 @@ class Explicit::Request
         part[1..-1].to_sym if part.start_with?(":")
       end
     end
+
+    def replace_path_params(values)
+      values.reduce(path) do |acc_path, (key, value)|
+        acc_path.gsub(":#{key}", value.to_s)
+      end
+    end
   end
 end
--- test/explicit/request/route_test.rb
+++ test/explicit/request/route_test.rb
@@ -12,4 +12,16 @@ class Explicit::Request::RouteTest < ActiveSupport::TestCase
     assert_equal [:id], Route.new("post", "/users/articles/:id").params
     assert_equal [:user_id, :id], Route.new("post", "/users/:user_id/articles/:id").params
   end
+
+  test "path_with_params" do
+    assert_equal(
+      "/articles/10",
+      Route.new("post", "/articles/:id").replace_path_params({ id: 10 })
+    )
+
+    assert_equal(
+      "/users/15/articles/10",
+      Route.new("post", "/users/:user_id/articles/:id").replace_path_params({ user_id: 15, id: 10 })
+    )
+  end
 end
