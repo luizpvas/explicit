ccfcb81e5a93305f099936a698f1ad0635257405
Luiz Vasconcellos <luizpvasc@gmail.com>
2025-01-02 13:51:12 -0300

    display integer constraints

--- app/helpers/explicit/application_helper.rb
+++ app/helpers/explicit/application_helper.rb
@@ -10,6 +10,16 @@ module Explicit
       render partial: "explicit/documentation/attribute", locals: { name:, type: }
     end
 
+    def type_constraints(&block)
+      content_tag(:div, class: "flex flex-wrap gap-2", &block)
+    end
+
+    def type_constraint(name, value)
+      content_tag(:div, class: "bg-neutral-200 px-1 text-sm") do
+        content_tag(:span, name) + " " + content_tag(:span, value)
+      end
+    end
+
     def type_has_details?(type)
       type.description.present? || type.has_details?
     end
