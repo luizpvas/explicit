2f7359e439b6c9e173ddb87ccb993df8f4b26d45
Luiz Vasconcellos <luizpvasc@gmail.com>
2025-01-04 12:02:08 -0300

    swagger 01

--- lib/explicit/documentation/output/swagger.rb
+++ lib/explicit/documentation/output/swagger.rb
@@ -8,8 +8,60 @@ module Explicit::Documentation::Output
       @builder = builder
     end
 
+    # top level attributes:
+    # swagger: 2.0
+    # info
+    # host
+    # basePath
+    # tags
+    # schemes
+    # paths
+    # securityDefinitions
+    # definitions
+    # externalDocs
+
+    def to_json
+      {
+        swagger: "2.0",
+        info: {
+          title: builder.get_page_title,
+        },
+        host: get_host_from_base_url,
+        basePath: get_base_path,
+        tags: build_tags_from_sections,
+        schemes: get_schemes_from_base_url
+      }
+    end
+
     def call
       raise NotImplementedError
     end
+
+    private
+      def get_host_from_base_url
+        url = builder.requests.first.get_base_url
+
+        # TODO: raise error if not set or inconsistent
+
+        URI(url).authority
+      end
+
+      def get_schemes_from_base_url
+        url = builder.requests.first.get_base_url
+
+        [URI(url).scheme]
+      end
+
+      def get_base_path
+        builder.requests.first.get_base_path
+
+        # TODO: raise error if inconsistent
+      end
+
+      def build_tags_from_sections
+        builder.sections.filter(&:contains_request?).map do |section|
+          { name: section.name }
+        end
+      end
   end
 end
---
+++ test/explicit/documentation/output/swagger_test.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Explicit::Documentation::Output::SwaggerTest < ActiveSupport::TestCase
+  test "version" do
+    assert_equal "2.0", swagger.to_json[:swagger]
+  end
+
+  test "info" do
+    assert_equal "Acme API", swagger.to_json.dig(:info, :title)
+  end
+
+  test "host" do
+    assert_equal "localhost:3000", swagger.to_json[:host]
+  end
+
+  test "basePath" do
+    assert_equal "/api/v1", swagger.to_json[:basePath]
+  end
+
+  test "tags" do
+    assert_equal "Auth", swagger.to_json.dig(:tags, 0, :name)
+    assert_equal "Articles", swagger.to_json.dig(:tags, 1, :name)
+    assert_equal "Others", swagger.to_json.dig(:tags, 2, :name)
+  end
+
+  test "schemes" do
+    assert_equal ["http"], swagger.to_json[:schemes]
+  end
+
+  private
+    def swagger
+      API::V1::Documentation.documentation_builder.swagger
+    end
+end
