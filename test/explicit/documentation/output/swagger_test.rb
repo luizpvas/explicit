ae24eee7024f86a403f475fd0bb8eb009d32783a
Luiz Vasconcellos <luizpvasc@gmail.com>
2025-01-04 13:14:41 -0300

    test refac

--- test/explicit/documentation/output/swagger_test.rb
+++ test/explicit/documentation/output/swagger_test.rb
@@ -4,33 +4,33 @@ require "test_helper"
 
 class Explicit::Documentation::Output::SwaggerTest < ActiveSupport::TestCase
   test "version" do
-    assert_equal "2.0", swagger.to_json[:swagger]
+    assert_equal "2.0", swagger[:swagger]
   end
 
   test "info" do
-    assert_equal "Acme API", swagger.to_json.dig(:info, :title)
+    assert_equal "Acme API", swagger.dig(:info, :title)
   end
 
   test "host" do
-    assert_equal "localhost:3000", swagger.to_json[:host]
+    assert_equal "localhost:3000", swagger[:host]
   end
 
   test "schemes" do
-    assert_equal ["http"], swagger.to_json[:schemes]
+    assert_equal ["http"], swagger[:schemes]
   end
 
   test "basePath" do
-    assert_equal "/api/v1", swagger.to_json[:basePath]
+    assert_equal "/api/v1", swagger[:basePath]
   end
 
   test "tags" do
-    assert_equal "Auth", swagger.to_json.dig(:tags, 0, :name)
-    assert_equal "Articles", swagger.to_json.dig(:tags, 1, :name)
-    assert_equal "Others", swagger.to_json.dig(:tags, 2, :name)
+    assert_equal "Auth", swagger.dig(:tags, 0, :name)
+    assert_equal "Articles", swagger.dig(:tags, 1, :name)
+    assert_equal "Others", swagger.dig(:tags, 2, :name)
   end
 
   private
     def swagger
-      API::V1::Documentation.documentation_builder.swagger
+      @swagger ||= API::V1::Documentation.documentation_builder.swagger.to_json
     end
 end
