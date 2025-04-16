2452be91e0cede2176b3930682b584ef59aca093
Luiz Vasconcellos <luizpvasc@gmail.com>
2025-04-16 09:54:15 -0300

    fix typo

--- test/dummy/app/controllers/api/v1/mcp_server.rb
+++ test/dummy/app/controllers/api/v1/mcp_server.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module API::V1
-  MCPServer = ::Explicit::MCPServer.new
+  MCPServer = ::Explicit::MCPServer.new do
     add API::V1::ArticlesController::IndexRequest
     add API::V1::ArticlesController::CreateRequest
     add API::V1::ArticlesController::ShowRequest
