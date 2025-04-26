d10397fa4552f1af6a4cf09065c5d6316f7acd7a
Luiz Vasconcellos <luizpvasc@gmail.com>
2025-04-26 16:55:26 -0300

    support empty initialization requests

--- lib/explicit/mcp_server/request.rb
+++ lib/explicit/mcp_server/request.rb
@@ -5,8 +5,6 @@ require "rack/utils"
 module Explicit::MCPServer
   Request = ::Data.define(:id, :method, :params, :host, :headers) do
     def self.from_rack_env(env)
-      body = ::JSON.parse(env["rack.input"].read)
-      
       headers = env.each_with_object({}) do |(key, value), hash|
         if key.start_with?("HTTP_") && key != "HTTP_HOST"
           header_name = key[5..-1].split("_").map(&:capitalize).join("-")
@@ -14,12 +12,22 @@ module Explicit::MCPServer
         end
       end
 
+      body = ::JSON.parse(env["rack.input"].read)
+
       new(
         id: body["id"],
         method: body["method"],
         params: body["params"],
         host: env["HTTP_HOST"],
         headers:
+      )
+    rescue ::JSON::ParserError
+      new(
+        id: nil,
+        method: nil,
+        params: nil,
+        host: env["HTTP_HOST"],
+        headers: headers
       )
     end
 
